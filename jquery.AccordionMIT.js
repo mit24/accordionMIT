3e152e4d7b64c8911544f2fe7a36bd62fbdbf06b
mit24 <a.pavlov@mit24.ru>
2013-07-03 07:33:12 -0700

    Update jquery.AccordionMIT.js
    Some bugs fix

--- jquery.AccordionMIT.js
+++ jquery.AccordionMIT.js
@@ -1,23 +1,23 @@
 (function($){
-  jQuery.fn.AccordionMIT = function(options){		
+	jQuery.fn.AccordionMIT = function(options){
 		options = $.extend({
-			parentBlockRowsDefault: 2,	//The number of rows that are visible when the plugin is loaded
-			parentPadding: 20,		//Indent button from parent
-			elementColsInRow: 3,		//The number of elements in a row
-			elementChild: ".child-element",	//Identifekator child element
-			toggle: true, 			//Show button after turning
-			toggleImg: "/ down.png", 	//Button image
-			toggleWidth: "100%", 		//width of the button
-			toggleHeight: 60, 		//height of the button
-			animateSpeed: 1000 		//animation speed
+			parentBlockRowsDefault: 2,		//Count rows, shows when plugin is loaded
+			parentPadding: 20,			//Margin button from parent block
+			elementColsInRow: 3,			//Numbers elements in row
+			elementChild: ".press-element",		//Child class in DOM structure
+			toggle: true,				//Show button after click
+			toggleImg: "/i/arrow/down.png",		//Button image
+			toggleWidth: "100%",			//Width button
+			toggleHeight: 60,			//Height кнопки
+			animateSpeed: 1000,			//Animation speed
 		}, options);
-		
-		//Check for integrality
+
+		//Check of integer
 		var isInteger = function(val) {
 			return (val ^ 0) === val;
 		}
-		
-		//controller size
+
+		//Controll of size
 		var controller = function(val) {
 			var totalCol = $(val).find(options.elementChild).length,
 				realRow = parseInt(totalCol/options.elementColsInRow);
@@ -26,70 +26,68 @@
 			}
 			else return false;
 		}
-		
+
 		//Assembly
 		var make = function(){
-				
-			var 	rowHeight = parseInt($(this).children(options.elementChild).css("height")) +			
-					parseInt($(this).children(options.elementChild).css("margin-top")) +
-					parseInt($(this).children(options.elementChild).css("margin-bottom"))+
-					parseInt($(this).children(options.elementChild).css("border-top-width"))+
-					parseInt($(this).children(options.elementChild).css("border-bottom-width"))+
-					parseInt($(this).children(options.elementChild).css("padding-top"))+
-					parseInt($(this).children(options.elementChild).css("padding-bottom")),
+			var element = $(this),
+				rowHeight = 	parseInt($(this).children(options.elementChild).css("height")) + 
+						parseInt($(this).children(options.elementChild).css("margin-top")) + 
+						parseInt($(this).children(options.elementChild).css("margin-bottom"))+	
+						parseInt($(this).children(options.elementChild).css("border-top-width"))+
+						parseInt($(this).children(options.elementChild).css("border-bottom-width"))+
+						parseInt($(this).children(options.elementChild).css("padding-top"))+
+						parseInt($(this).children(options.elementChild).css("padding-bottom")),
 				sizeBlockDefault = rowHeight*options.parentBlockRowsDefault,
 				keyBlock = (Math.random() * (200 - 1) + 1).toFixed(0);
-			
-			//Formation of the parent unit
-			$(this).addClass("uBankAccordion"+keyBlock)		//Uniq key of block
-			.addClass('deactiveToggleU') 				//Class presence of an active unit
-			.attr("data-height-default",sizeBlockDefault);		//Saving the height of default attribute
-			
+
+			//Make parent block
+			$(this).addClass("uBankAccordion"+keyBlock)
+			.addClass('deactiveToggleU') 
+			.attr("data-height-default",sizeBlockDefault);
+
 			var sizeControl = controller(".uBankAccordion"+keyBlock);
 			if(sizeControl==false) {
 				$(".uBankAccordion"+keyBlock).css("height",sizeBlockDefault);
+				$(this).after($('<div />')									//Создание кнопки переключателя
+						.addClass('toggle')											//Присвоение класса
+						.css({
+							"display"	: "block",
+							"position"	: "relative",
+							"bottom"	: 0,
+							"width"		: options.toggleWidth,
+							"height"	: options.toggleHeight,
+							"background-image": "url("+options.toggleImg+")",
+							"background-position": "center",
+							"background-repeat": "no-repeat",
+							"cursor"	:"pointer"
+						})															//Дизайн кнопки переключателя
+						.attr("rel",".uBankAccordion"+keyBlock)						//Установка ключа родительского блока
+						.click(function() {
+							var toggleSpan = $(this),
+								relToggle = $(this).attr("rel"),
+								countDivs = $(relToggle).find(options.elementChild).length,
+								rowsCount = countDivs/options.elementColsInRow;
+
+							if($(relToggle).hasClass("deactiveToggleU")) {
+								if(isInteger(rowsCount)==true) {
+									sizeBlock = rowsCount*rowHeight+options.parentPadding;
+								}
+								else {
+									var intRowsCount = parseInt(rowsCount);
+									sizeBlock = ((intRowsCount + 1) * rowHeight) + options.parentPadding;
+								}
+								if(options.toggle==false) {
+									$(this).fadeOut(options.animateSpeed);
+								}
+								$(relToggle).animate({height:sizeBlock},options.animateSpeed).removeClass("deactiveToggleU").addClass("activeToggleU");
+								toggleSpan.addClass("up-img");
+							}
+							else {
+								$(relToggle).animate({height:sizeBlockDefault},options.animateSpeed).removeClass("activeToggleU").addClass("deactiveToggleU");
+								toggleSpan.removeClass("up-img");
+							}
+						}));
 			}
-			else {
-				//options.toggle=false;
-			}
-			
-			//Making button switch
-			$(this).after($('<div />')
-			.addClass('toggle')
-			.css({
-				"display"	: "block",
-				"position"	: "relative",
-				"bottom"	: 0,
-				"width"		: options.toggleWidth,
-				"height"	: options.toggleHeight,
-				"background-image": "url("+options.toggleImg+")",
-				"background-position": "center",
-				"background-repeat": "no-repeat",
-				"cursor"	:"pointer"
-			})
-			.attr("rel",".uBankAccordion"+keyBlock)
-			.click(function() {
-				var 	relToggle = $(this).attr("rel"),
-					countDivs = $(relToggle).find(options.elementChild).length,
-					rowsCount = countDivs/options.elementColsInRow;
-				
-				if($(relToggle).hasClass("deactiveToggleU")) {	
-					if(isInteger(rowsCount)==true) {
-						sizeBlock = rowsCount*rowHeight+options.parentPadding;
-					}
-					else {
-						var intRowsCount = parseInt(rowsCount);
-						sizeBlock = ((intRowsCount + 1) * rowHeight) + options.parentPadding;
-					}
-					if(options.toggle==false) {
-						$(this).fadeOut(options.animateSpeed);
-					}
-					$(relToggle).animate({height:sizeBlock},options.animateSpeed).removeClass("deactiveToggleU").addClass("activeToggleU");
-				}
-				else {
-					$(relToggle).animate({height:sizeBlockDefault},options.animateSpeed).removeClass("activeToggleU").addClass("deactiveToggleU");
-				}
-			}));
 	    };
 
 	    return this.each(make);
